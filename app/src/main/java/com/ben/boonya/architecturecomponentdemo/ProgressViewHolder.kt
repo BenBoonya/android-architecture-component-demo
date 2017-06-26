69189398f1e6254643c7589091a4d7f9cd396498
Oozou <boonya.kitpitak@gmail.com>
2017-06-26 17:16:48 +0700

    set up progress viewholder

--- app/src/main/java/com/ben/boonya/architecturecomponentdemo/ProgressViewHolder.kt
+++ app/src/main/java/com/ben/boonya/architecturecomponentdemo/ProgressViewHolder.kt
@@ -1,5 +1,11 @@
-package com.ben.boonya.architecturecomponentdemo.characterlist
+package com.ben.boonya.architecturecomponentdemo
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
 
 /**
  * Created by oozou on 6/26/2017 AD.
  */
+class ProgressViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val progressBar by lazy { view.findViewById(R.id.progressBar) }
+}
