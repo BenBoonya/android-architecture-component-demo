74a66667df49e86e12af134194e2e5e5c7195c69
jutikorn <jutikorn.v@gmail.com>
2017-06-26 19:10:37 +0700

    -using property Delegate

--- app/src/main/java/com/ben/boonya/architecturecomponentdemo/extensions/RecyclerView.kt
+++ app/src/main/java/com/ben/boonya/architecturecomponentdemo/extensions/RecyclerView.kt
@@ -2,38 +2,40 @@ package com.ben.boonya.architecturecomponentdemo.extensions
 
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import junit.framework.Assert
+import kotlin.reflect.KProperty
 
 /**
  * Created by jutikornvarojananulux on 6/25/2017 AD.
  */
-fun RecyclerView.onLoadMoreListener(onLoadMore: (Int) -> Unit) {
-    layoutManager?.let {
-        this.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+val RecyclerView.visibleThreshold : Int get() = 5
 
-            var previousTotal = 0
-            var loading = true
-            var visibleThreshold = 5
-            var firstVisibleItem = 0
-            var visibleItemCount = 0
-            var totalItemCount = 0
-            var currentPage = 1
+val LinearLayoutManager.firstVisibleItem : Int get() = findFirstVisibleItemPosition()
+
+
+var RecyclerView.previousTotal: Int by NumberDelegate()
+var RecyclerView.currentPage: Int by NumberDelegate()
+var RecyclerView.loading: Boolean by BooleanDelegate()
+
+
+fun RecyclerView.onLoadMoreListener(startPage: Int = 1, onLoadMore: (Int) -> Unit) {
+    this.currentPage = startPage
+    this.previousTotal = 0
+    this.loading = true
+    this.layoutManager?.let {
+        this.addOnScrollListener(object : RecyclerView.OnScrollListener() {
 
             override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
-                visibleItemCount = childCount
-                totalItemCount = layoutManager.itemCount
-                firstVisibleItem = (layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+                val firstVisibleItem = (layoutManager as LinearLayoutManager).firstVisibleItem
 
-                if (loading && totalItemCount > previousTotal) {
+                if (loading && layoutManager.itemCount > previousTotal) {
                     loading = false
-                    previousTotal = totalItemCount
+                    previousTotal = layoutManager.itemCount
                 }
 
-                if (!loading && (totalItemCount - visibleItemCount)
+                if (!loading && (layoutManager.itemCount - childCount)
                         <= (firstVisibleItem + visibleThreshold)) {
                     // End has been reached
-
                     // Do something
                     currentPage++
 
@@ -44,5 +46,37 @@ fun RecyclerView.onLoadMoreListener(onLoadMore: (Int) -> Unit) {
             }
 
         })
+    }
+}
+
+fun RecyclerView.resetLoadMoreState(startPage: Int = 1) {
+    this.currentPage = startPage
+    this.previousTotal = 0
+    this.loading = true
+}
+
+class NumberDelegate {
+
+    var number: Int? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+        return number?:0
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+        number = value
+    }
+}
+
+class BooleanDelegate {
+
+    var myBoolean: Boolean? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): Boolean {
+        return myBoolean?:true
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Boolean) {
+        myBoolean = value
     }
 }
